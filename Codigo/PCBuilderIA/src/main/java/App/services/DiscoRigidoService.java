4b07555dfb25fbcb8855b311503384175b125825
tcedro <thiago.cedro@sga.pucminas.brgit config --global user.name tcedrogit config --global user.email thiago.cedro@sga.pucminas.br>
2023-11-24 09:19:15 -0300

    final

--- Codigo/PCBuilderIA/src/main/java/App/services/DiscoRigidoService.java
+++ Codigo/PCBuilderIA/src/main/java/App/services/DiscoRigidoService.java
@@ -3,7 +3,6 @@ package App.services;
 import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
--- Codigo/PCBuilderIA/target/classes/frontend/scriptMontagem.js
+++
@@ -1,2 +0,0 @@
-const pokemonList = document.getElementById('pokemonList')
-
